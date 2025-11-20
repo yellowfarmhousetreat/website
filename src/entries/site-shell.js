8052fc6dcffde72095323a3a3c78723684215709
yellowfarmhousetreats <brian.dotson@me.com>
2025-11-20 09:40:51 -0700

    fix: update overlay menu import to use the correct file

--- src/entries/site-shell.js
+++ src/entries/site-shell.js
@@ -14,7 +14,7 @@
 // Import third-party dependencies
 
 // Import core functionality (ISOLATED - won't break from other changes)
-import '../../assets/js/overlay-menu-simple.js';
+import '../../assets/js/overlay-menu.js';
 
 // Import page-specific modules
 import '../../assets/js/product-card-info.js';
